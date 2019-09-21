bfc91a847d50d7352db8b4174ddd8e5f0b200230
Frank Roeder <frankyroeder@icloud.com>
2019-09-21 11:28:49 +0200

    Linting and remove dev dependency

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const express = require('express')
 const bodyParser = require('body-parser')
+const express = require('express')
 const fs = require('fs')
 
 const app = express()
 const port = process.env.PORT || 4000
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(express.static(__dirname + `/public`))
 
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
-
 app.post('/data', function (req, res) {
   console.log(req.body)
   const writeObj = {
@@ -27,7 +26,7 @@ app.post('/data', function (req, res) {
     "drawing": req.body.ink
   }
   fs.appendFile('test.ndjson', `${JSON.stringify(writeObj)}\n`, function (err) {
-    if(err) {
+    if (err) {
       throw err
     }
     res.send()
