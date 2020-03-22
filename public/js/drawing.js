442355ddf12b8864f1065445fdf51bb764ab4442
Ray Subahri <raysubahri@posteo.de>
2020-03-22 15:52:49 +0100

    Übersichtlichkeit verbessert Liste an Funktionen vorangestellt Pinselgröße als Objektmethode zugefügt Überflüssige Zeilen gelöscht

--- public/js/drawing.js
+++ public/js/drawing.js
@@ -2,72 +2,11 @@
 var path, ink, scores, imgData
 var timer = 0, lastTimestamp = 0, lastTimestamp_check = 0, idx_guess = 0
 var d_scores = {}
-var selectedWidth = 2
 
-paper.install(window)
-
-window.onload = function() {
-
-  initInk()
-  paper.setup('canvas')
-  var tool = new Tool()
-  // The mouse has to drag at least 10pt
-  // before the next drag event is fired:
-  tool.minDistance = 3.5;
-
-  // Paper Tool Mouse Down Event
-  tool.onMouseDown = function(event) {
-    if (path) {
-    };
-    path = new Path({
-      segments: [event.point],
-      strokeColor: 'black',
-      strokeWidth: selectedWidth,
-      strokeCap: 'round',
-      strokeJoin: 'round'
-  });
-    
-    
-    // Get Time [ms] for each Guess (needed for accurate Google AI Guessing)
-    var thisTimestamp = event.event.timeStamp;
-    if(timer === 0) {
-      timer = 1;
-      var time = 0;
-    } else {
-      var timeDelta = thisTimestamp - lastTimestamp;
-      var time = ink[2][ink[2].length-1] + timeDelta;
-    }
-
-    // Get XY point from event w/ time [ms] to update Ink Array
-    updateInk(event.point, time)
-    // Draw XY point to Paper Path
-    path.add(event.point)
-    // Reset Timestamps
-    lastTimestamp = thisTimestamp;
-  }
-
-  tool.onMouseDrag = function(event) {
-    // Get Event Timestamp and Timestamp Delta
-    var thisTimestamp = event.event.timeStamp
-    var timeDelta = thisTimestamp - lastTimestamp
-    // Get new Time for Ink Array
-    var time = ink[2][ink[2].length-1] + timeDelta;
-    // Get XY point from event w/ time [ms] to update Ink Array
-    updateInk(event.point, time)
-    // Draw XY point to Paper Path
-    path.add(event.point)
-    // Reset Timestamps
-    lastTimestamp = thisTimestamp
-  }
-  tool.onMouseUp = function(event) {
-    path.simplify();
-  }  
+// Initialize Functions
+function initInk() {
+  ink = [[], [], []]
 }
-
-function initInk(){
-  ink = [[],[],[]]
-}
-
 function clearDrawing() {
   // Remove Paper Path Layer
   paper.project.activeLayer.removeChildren()
@@ -77,33 +16,29 @@ function clearDrawing() {
   idx_guess = 0
   d_scores = {}
 }
-
 function saveDrawing() {
   // Get Paper Canvas Weight/Height
   var c_dims = getCanvasDimensions()
   var e = document.getElementById("dropDown")
   var label = e.options[e.selectedIndex].value
   imgData = {
-    "dims": {"width": c_dims.width, "height":c_dims.height},
+    "dims": { "width": c_dims.width, "height": c_dims.height },
     "label": label,
     "ink": [ink]
   }
   clearDrawing()
 }
-
 // Update Ink Array w/ XY Point + Time
-function updateInk(point, time){
+function updateInk(point, time) {
   ink[0].push(point.x)
   ink[1].push(point.y)
   ink[2].push(time)
 }
-
-function getCanvasDimensions(){
+function getCanvasDimensions() {
   var w = document.getElementById('canvas').offsetWidth
   var h = document.getElementById('canvas').offsetHeight
-  return {height: h, width: w}
+  return { height: h, width: w }
 }
-
 // Create and Fill Array
 function createArray(len, itm) {
   var arr1 = [itm], arr2 = []
@@ -113,4 +48,64 @@ function createArray(len, itm) {
     len >>>= 1
   }
   return arr2
+}
+
+
+
+// Run paper.js
+paper.install(window)
+
+// Set mouse properties
+window.onload = function () {
+
+  initInk()
+  paper.setup('canvas')
+  var tool = new Tool()
+  // The mouse has to drag at least 3.5pt
+  // before the next drag event is fired:
+  tool.minDistance = 3.5;
+
+  // Paper Tool Mouse Down Event
+  tool.onMouseDown = function (event) {
+    path = new Path({
+      segments: [event.point],
+      strokeColor: 'black',
+      strokeWidth: 2,
+      strokeCap: 'round',
+      strokeJoin: 'round',
+    });
+    // Get Time [ms] for each Guess (needed for accurate Google AI Guessing)
+    var thisTimestamp = event.event.timeStamp;
+    if (timer === 0) {
+      timer = 1;
+      var time = 0;
+    } else {
+      var timeDelta = thisTimestamp - lastTimestamp;
+      var time = ink[2][ink[2].length - 1] + timeDelta;
+    }
+    // Get XY point from event w/ time [ms] to update Ink Array
+    updateInk(event.point, time)
+    // Draw XY point to Paper Path
+    path.add(event.point)
+    // Reset Timestamps
+    lastTimestamp = thisTimestamp;
+  }
+
+  tool.onMouseDrag = function (event) {
+    // Get Event Timestamp and Timestamp Delta
+    var thisTimestamp = event.event.timeStamp
+    var timeDelta = thisTimestamp - lastTimestamp
+    // Get new Time for Ink Array
+    var time = ink[2][ink[2].length - 1] + timeDelta;
+    // Get XY point from event w/ time [ms] to update Ink Array
+    updateInk(event.point, time)
+    // Draw XY point to Paper Path
+    path.add(event.point)
+    // Reset Timestamps
+    lastTimestamp = thisTimestamp
+  }
+
+  tool.onMouseUp = function (event) {
+    path.simplify();
+  }
 }
